bee7c60b788d612cf1b457f074ab91010cb1849b
Brandon Stottler <bstottler11@gmail.com>
2020-04-08 11:29:46 -0700

    outlined workflow

--- pomodoro.js
+++ pomodoro.js
@@ -1,3 +1,9 @@
 //global variables
 
-//create incrementing function
+//create incrementing function; puts timer value in title as well as 'working' or 'breaking'
+
+//add functionality to change break time and work time
+
+//add functionality to start, pause, stop, and reset buttons
+
+//add break start noise, add work start noise
